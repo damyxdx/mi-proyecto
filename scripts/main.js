2cb3d4ff25da51961936653596ad60ca9cbe35f0
damyxdx <damyxdx12@gmail.com>
2024-08-01 21:30:27 -0300

    damyx

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+// scripts/main.js
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.querySelector('.button');
+    button.addEventListener('click', () => {
+        alert('¡Botón clicado!');
+    });
+});
